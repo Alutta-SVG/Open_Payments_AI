045048bdb13de5ab717d2ed8c1abeb1ae58a71be
progress <leswifil@gmail.com>
2024-09-22 14:46:22 +0200

    removed files

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import bodyParser from "body-parser";
 import flash from "connect-flash";
 import session from "express-session";
 import home from "./routes/home.js";
-import shoes from "./routes/shoes.js";
+
 
 
 //route instances
 const home_route = home()
-const shoes_route = shoes()
+
 
 //expressJS instance
 const app = express();
@@ -17,7 +17,7 @@ const app = express();
 //middleware configuration
 app.use(
     session({
-        secret: "shoe catalogue api",
+        secret: "Cash One",
         resave: false,
         saveUninitialized: true,
     })
@@ -37,11 +37,7 @@ app.use(function (req, res, next) {
 
 //screen routes
 app.get('/', home_route.home_page);
-app.get('/shoes', shoes_route.shoes_page)
-app.get('/shoes/brand/:brandname', shoes_route.get_brand)
-app.get('/shoes/brand/:size', shoes_route.get_size)
-app.get('/shoes/brand/:brandname/size/:size', shoes_route.get_size_and_brand)
-app.get('/shoes/filter', shoes_route.filterShoes);
+
  
 // Start the server
 const PORT = process.env.PORT || 3007;
